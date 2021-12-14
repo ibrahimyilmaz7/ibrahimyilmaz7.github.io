95b12cfd85c19658551e8117cbdf822ee6a52a2c
IBRAHIM YILMAZ <0ibrahimyilmaz@gmail.com>
2021-12-14 18:34:21 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,12 +4,13 @@ const app = new App({
 	target: document.body,
 	props: {
 		name: 'Ibrahim Yilmaz',
-		position: 'Senior iOS Developer',
-		location: 'Izmir/Turkey',
+		position: 'Senior iOS Developer @ CHECK24',
+		location: 'Frankfurt/Germany',
 		twitterHandle: 'iboylmz',
 		githubHandle: 'ibrahimyilmaz7',
 		linkedinHandle: 'ibrahimyilmaz7',
-		stackOverflowHandle: '2696626'
+		stackOverflowHandle: '2696626',
+		summary: 'I have almost %d years of experience. I\'m a "Natural Born Engineer" and I have an open personality for improvement. I love following new technologies, and enjoy writing code. <br/>Currently, I\'m working as a Senior iOS Developer at CHECK24 Bank.<br/> I have significant banking/finance experience.'
 	}
 });
 
